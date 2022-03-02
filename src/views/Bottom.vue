8fb180694de2daec9dc9dd78f94d3c74dfd6509d
asens <asensts@163.com>
2022-03-02 17:10:38 +0800

    new vant_app branch

--- src/views/Bottom.vue
+++ src/views/Bottom.vue
@@ -1,10 +1,17 @@
 <template>
-  Bottom
+  <van-row class="bottom">
+    <van-col span="24"></van-col>
+  </van-row>
 </template>
 
 <script setup>
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@styles/mixin.scss';
 
+.bottom {
+  @include line-height(50px, 50px);
+  @include position(fixed, $bottom: 0)
+}
 </style>
--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>Header</div>
+<div class="header">
+  <van-row class="header-top">
+    <van-col span="24">
+      <i class="iconfont icon-search1"></i>
+    </van-col>
+  </van-row>
+  <Navigation></Navigation>
+</div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import Navigation from '@components/navigation'
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@styles/mixin.scss';
 
+.header {
+  .header-top {
+    @include line-height(45px, 45px);
+  }
+}
 </style>
