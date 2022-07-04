734ba0c42798fc5c01b94e44c871565fba143d3a
asensts <42834560+AsensTs@users.noreply.github.com>
2022-07-04 14:23:26 +0800

    Update schedulingPar.js

--- src/apis/schedulingPar.js
+++ src/apis/schedulingPar.js
@@ -1,10 +1,5 @@
 import axios from '@utils/axios';
 
- //调度操作票扫描
-//  predictate_ticket: '/api/predictate/index', // table
-//  predictate_ticket_details: '/api/predictate/details/', // 详情
-//  predictate_ticket_again_check: "/api/predictate/again_check/", //重新扫描
-// message_search: '/api/message/index', // 短信查询
 
 export function predictateTicket(params) {
   return axios({
